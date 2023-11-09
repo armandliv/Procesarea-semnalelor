c711fa3462dabea4a26d9338bc5f261fe4e879ec
unknown <armi_liv_hh@yahoo.com>
2023-11-09 17:45:41 +0200

    Lab5 - prezenta

--- Lab5/1.py
+++ Lab5/1.py
@@ -1,8 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.genfromtxt('Lab5/Train.csv', delimiter=',')
-x = x[1:]
+file = open('Lab5/Train.csv', 'r')
+data = []
+for line in file.readlines()[1:]:
+    data += [line.strip().split(',')]
+x = [int(y[2]) for y in data]
+N = len(x)
+
 
 ### a)
 # Frecventa de esantionare este de 1 data pe ora <=> 1/3600 ori per secunda <=> 1/3600 Hz <=> 0.0002777777 Hz
@@ -13,7 +18,19 @@ x = x[1:]
 ### c)
 # Frecventa maxima prezenta in semnal ar fi 0.0002777777 /2 = 0.0001388885
 
-fvmax = max(x[:,2])
+fvmax = max(x)/len(x)/3600
 print(fvmax)
 
 ### d)
+X = np.fft.fft(x)
+X = abs(X/N)
+X = X[:N//2]
+fs = 1/3600
+f = fs*np.linspace(0,N/2,N//2)/N;
+plt.plot(f,X)
+plt.savefig("Lab5/grafice/d.pdf", format="pdf")
+plt.savefig("Lab5/grafice/d.png", format="png")
+plt.show()
+
+
+### e)
